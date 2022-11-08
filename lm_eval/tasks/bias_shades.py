6c64b6d1936fd3a6a54f837c257232133f4a9f45
jon-tow <jonathantow1@gmail.com>
2022-11-07 21:03:21 -0500

    Add bias-shades tasks to registry

--- lm_eval/tasks/bias_shades.py
+++ lm_eval/tasks/bias_shades.py
@@ -1,207 +1,71 @@
 """
 Multilingual dataset for measuring social biases in language models.
 https://huggingface.co/datasets/BigScienceBiasEval/bias-shades/viewer/spanish/test
+
+TODO: Add `arabic`, `german`, `russian`, and `tamil` subsets when `promptsource`
+templates become available.
 """
-from lm_eval.base import PromptSourceTask
+from lm_eval.api.task import PromptSourceTask
 
 
 _CITATION = """"""
 
 
-class ShadesEnglish(PromptSourceTask):
+class BiasShadesBase(PromptSourceTask):
+    def has_training_docs(self):
+        return False
+
+    def has_validation_docs(self):
+        return False
+
+    def has_test_docs(self):
+        return True
+
+    def training_docs(self):
+        pass
+
+    def validation_docs(self):
+        pass
+
+    def test_docs(self):
+        if self.has_test_docs():
+            return self.dataset["test"]
+
+
+class BiasShadesEnglish(BiasShadesBase):
     VERSION = 0
     DATASET_PATH = "BigScienceBiasEval/bias-shades"
     DATASET_NAME = "english"
 
-    def has_training_docs(self):
-        return False
 
-    def has_validation_docs(self):
-        return False
-
-    def has_test_docs(self):
-        return True
-
-    def training_docs(self):
-        pass
-
-    def validation_docs(self):
-        pass
-
-    def test_docs(self):
-        if self.has_test_docs():
-            return self.dataset["test"]
-
-
-class ShadesFrench(PromptSourceTask):
+class BiasShadesFrench(BiasShadesBase):
     VERSION = 0
     DATASET_PATH = "BigScienceBiasEval/bias-shades"
     DATASET_NAME = "french"
 
-    def has_training_docs(self):
-        return False
 
-    def has_validation_docs(self):
-        return False
-
-    def has_test_docs(self):
-        return True
-
-    def training_docs(self):
-        pass
-
-    def validation_docs(self):
-        pass
-
-    def test_docs(self):
-        if self.has_test_docs():
-            return self.dataset["test"]
-
-
-class ShadesHindi(PromptSourceTask):
+class BiasShadesHindi(BiasShadesBase):
     VERSION = 0
     DATASET_PATH = "BigScienceBiasEval/bias-shades"
     DATASET_NAME = "hindi"
 
-    def has_training_docs(self):
-        return False
 
-    def has_validation_docs(self):
-        return False
-
-    def has_test_docs(self):
-        return True
-
-    def training_docs(self):
-        pass
-
-    def validation_docs(self):
-        pass
-
-    def test_docs(self):
-        if self.has_test_docs():
-            return self.dataset["test"]
-
-
-# class ShadesArabic(PromptSourceTask):
-#     VERSION = 0
-#     DATASET_PATH = "BigScienceBiasEval/bias-shades"
-#     DATASET_NAME = "arabic"
-
-#     def has_training_docs(self):
-#         return False
-
-#     def has_validation_docs(self):
-#         return False
-
-#     def has_test_docs(self):
-#         return True
-
-#     def training_docs(self):
-#         pass
-
-#     def validation_docs(self):
-#         pass
-
-#     def test_docs(self):
-#         if self.has_test_docs():
-#             return self.dataset["test"]
-
-
-class ShadesSpanish(PromptSourceTask):
+class BiasShadesSpanish(BiasShadesBase):
     VERSION = 0
     DATASET_PATH = "BigScienceBiasEval/bias-shades"
     DATASET_NAME = "spanish"
 
-    def has_training_docs(self):
-        return False
 
-    def has_validation_docs(self):
-        return False
-
-    def has_test_docs(self):
-        return True
-
-    def training_docs(self):
-        pass
-
-    def validation_docs(self):
-        pass
-
-    def test_docs(self):
-        if self.has_test_docs():
-            return self.dataset["test"]
+BIAS_SHADES_CLASSES = [
+    BiasShadesEnglish,
+    BiasShadesFrench,
+    BiasShadesHindi,
+    BiasShadesSpanish,
+]
 
 
-# class ShadesRussian(PromptSourceTask):
-#     VERSION = 0
-#     DATASET_PATH = "BigScienceBiasEval/bias-shades"
-#     DATASET_NAME = "russian"
-
-#     def has_training_docs(self):
-#         return False
-
-#     def has_validation_docs(self):
-#         return False
-
-#     def has_test_docs(self):
-#         return True
-
-#     def training_docs(self):
-#         pass
-
-#     def validation_docs(self):
-#         pass
-
-#     def test_docs(self):
-#         if self.has_test_docs():
-#             return self.dataset["test"]
-
-# class ShadesGerman(PromptSourceTask):
-#     VERSION = 0
-#     DATASET_PATH = "BigScienceBiasEval/bias-shades"
-#     DATASET_NAME = "german"
-
-#     def has_training_docs(self):
-#         return False
-
-#     def has_validation_docs(self):
-#         return False
-
-#     def has_test_docs(self):
-#         return True
-
-#     def training_docs(self):
-#         pass
-
-#     def validation_docs(self):
-#         pass
-
-#     def test_docs(self):
-#         if self.has_test_docs():
-#             return self.dataset["test"]
-
-
-# class ShadesTamil(PromptSourceTask):
-#     VERSION = 0
-#     DATASET_PATH = "BigScienceBiasEval/bias-shades"
-#     DATASET_NAME = "tamil"
-
-#     def has_training_docs(self):
-#         return False
-
-#     def has_validation_docs(self):
-#         return False
-
-#     def has_test_docs(self):
-#         return True
-
-#     def training_docs(self):
-#         pass
-
-#     def validation_docs(self):
-#         pass
-
-#     def test_docs(self):
-#         if self.has_test_docs():
-#             return self.dataset["test"]
+def construct_tasks():
+    tasks = {}
+    for bias_shades_class in BIAS_SHADES_CLASSES:
+        tasks[f"bias_shades_{bias_shades_class.DATASET_NAME}"] = bias_shades_class
+    return tasks
